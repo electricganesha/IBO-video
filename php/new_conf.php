c7ca42a694620762170726f7b27227289e427bd2
Unknown <ricardoadspinto@gmail.com>
2016-12-07 17:14:23 +0000

    Resolvido erro de eliminar os videos offline

--- php/new_conf.php
+++ php/new_conf.php
@@ -12,7 +12,7 @@
 		$mysqli->query("INSERT INTO tb_conferencias (nome, orador, estado, dia, hour, peer_id) VALUES ('$conf_name','$fullname', 'live', '$dia', '$hora', '$id_conf')");
 		//$mysqli->query("CREATE TABLE $id_conf AS SELECT * FROM cadeiras");
 	}else if($state == 'disconnected'){
-		$mysqli->query("DELETE FROM tb_conferencias WHERE peer_id = '$id_conf'");
+		$mysqli->query("DELETE FROM tb_conferencias WHERE peer_id = '$id_conf' AND estado != 'offline'");
 		//$mysqli->query("DROP TABLE $id_conf");
 	}
 	$mysqli->close();
